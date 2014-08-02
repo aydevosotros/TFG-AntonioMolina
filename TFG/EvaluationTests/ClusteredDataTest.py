cd9911e4708966bdc93f38617143b5a42289e82c
antonio <antonio@antonio-N53SV>
2014-08-02 09:30:30 +0200

    Documentation files added

---
+++ TFG/EvaluationTests/ClusteredDataTest.py
@@ -0,0 +1,18 @@
+'''
+Created on Jul 28, 2014
+
+@author: antonio
+'''
+
+class ClusteredDataTest(object):
+    '''
+    Esta clase preparara el entorno, ejecutara pruebas de clasisficacion sobre M elementos
+    que siguen una distribucion gausiana alrededor de un numero N de centroides aleatorios.
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
--- TFG/EvaluationTests/test.py
+++ TFG/EvaluationTests/test.py
@@ -3,6 +3,9 @@ Created on Jul 28, 2014
     By executing this file you'll be executing and evaluating the test written in this package
 @author: antonio
 '''
+from EvaluationTests.ClusteredDataTest import ClusteredDataTest
 
 if __name__ == '__main__':
-    pass
+    print 'Performing test over the RBFNNs'
+    # Performing test based on clustered data
+    cdt = ClusteredDataTest(centroids=3, samples=100)
