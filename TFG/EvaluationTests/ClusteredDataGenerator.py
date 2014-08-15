02326d66481bed3d3ed8f79d1cff6a0f05a65d9c
antonio <antonio@kali.Home>
2014-08-15 15:09:53 +0200

    Reportes pdf automáticos y algunos cambios en general

--- TFG/EvaluationTests/ClusteredDataGenerator.py
+++ TFG/EvaluationTests/ClusteredDataGenerator.py
@@ -20,14 +20,15 @@ class ClusteredDataGenerator(object):
         Constructor
         '''
         # Params setting
-        self.nSamples = nSamples
+        self.totalSamples = nSamples
+        self.nSamples = nSamples / nCentroids
         self.nCentroids = nCentroids
         self.indim = indim
         self._generateData()
         
     def _generateData(self):
         for i in range(self.nCentroids):
-            mean = np.random.rand(2)*10 + (np.random.rand(2)*10)
+            mean = np.random.rand(2)*30 + (np.random.rand(2)*10)
             print "Creating cluster with center: " + str(mean)
             samples = np.random.normal(size=[self.nSamples, 2], loc=mean)
             n=0
@@ -42,24 +43,48 @@ class ClusteredDataGenerator(object):
     
     def plotDataSet(self):
         colors = ["red", "blue" , "green", "orange", "purple"]
-        for i in range(len(self.getX())):
-            plt.plot(self.getX()[i,0], self.getX()[i,1], "o", color=colors[int(self.getY()[i])])
+        ts = self.getTrainingX()
+        for i in range(len(self.getTrainingX())):
+            plt.plot(ts[i,0], ts[i,1], "o", color=colors[int(self.getTrainingY()[i])])
         plt.title("Plot of two first features of each sample in the dataset")
         plt.xlabel("First dimension")
         plt.ylabel("Second dimension")
         plt.show()
         
-    def getX(self):
-        return self.data[:,:-1]
+    def savePlot(self):
+        plt.clf()
+        colors = ["red", "blue" , "green", "orange", "purple"]
+        ts = self.getTrainingX()
+        for i in range(len(self.getTrainingX())):
+            plt.plot(ts[i,0], ts[i,1], "o", color=colors[int(self.getTrainingY()[i])])
+        plt.title("Plot of two first features of each sample in the dataset")
+        plt.xlabel("First dimension")
+        plt.ylabel("Second dimension")
+        plt.savefig("plots/"+str(self.nCentroids)+"c"+str(self.totalSamples)+"s"+".png")
         
-    def getY(self):
-        return self.data[:,-1]
+    def getTrainingX(self):
+        '''It returns the first 75% of the X in dataSet for training'''
+        print "El trainingSet tiene: ", len(self.data[:int(0.75*(self.nCentroids*self.nSamples)),:-1])
+        return self.data[:int(0.75*(self.nCentroids*self.nSamples)),:-1]
+        
+    def getTrainingY(self):
+        '''It returns the first 75% of the Y in dataSet for training'''
+        return self.data[:int(0.75*(self.nCentroids*self.nSamples)),-1]
+    
+    def getValidationX(self):
+        print "El validationTest tiene: ", len(self.data[int(0.75*(self.nCentroids*self.nSamples))+1:,:-1])
+        '''It returns the last 25% of the X in dataSet for verifying'''
+        return self.data[int(0.75*(self.nCentroids*self.nSamples))+1:,:-1]
+    
+    def getValidationY(self):
+        '''It returns the last 25% of the X in dataSet for verifying'''
+        return self.data[int(0.75*(self.nCentroids*self.nSamples)+1):,-1]
         
     def getDataSet(self):
-        return self.getX(), self.getY()
+        return self.getTrainingX(), self.getTrainingY()
             
     def verifyResult(self, Y):
-        print Y
+        vY = self.getValidationY()
         hits = 0.0
         fails = 0.0
         truePositive = 0
@@ -67,19 +92,21 @@ class ClusteredDataGenerator(object):
         trueNegative = 0
         falseNegative = 0
         for cy, y in enumerate(Y):
-            if y == self.data[cy,-1]:
+            if y == vY[cy]:
                 hits +=1
             else:
                 fails +=1
         
         #Printing results
         print "Hits: ", hits
-        print "Fais: ", fails
+        print "Fais: ", fails        
+        print "Performance", hits/len(vY)
         
-        print "Performance", hits/(self.nSamples*self.nCentroids)
+        return hits/len(vY)*100
         
 # Some code to debug
 if __name__ == '__main__':
-    cdt = ClusteredDataGenerator(100, 3, 2)
+    cdt = ClusteredDataGenerator(1000, 3, 2)
     cdt.plotDataSet()
     print cdt.getDataSet()
+    print len(cdt.getTrainingX())+len(cdt.getValidationX())
--- TFG/EvaluationTests/test.py
+++ TFG/EvaluationTests/test.py
@@ -6,12 +6,91 @@ Created on Jul 28, 2014
 from EvaluationTests.ClusteredDataGenerator import ClusteredDataGenerator
 from basicRBFNN.RBFNN import RBFNN
 import numpy as np
+import time
+import shlex, subprocess
+from matplotlib import pyplot as plt
+
+
+def generatePdfReport(nCentroids, perf, tTime):
+    #TODO: Create the script for creating report
+    pass
+
+def ClusteredDataTest():
+    latexReport = '''
+    \\documentclass[10pt,a4paper, twocolumn]{report}
+    \\usepackage[T1]{fontenc} 
+    \\usepackage[utf8]{inputenc} 
+    \\usepackage[spanish]{babel}
+    \\usepackage{amsmath}
+    \\usepackage{amsfonts}
+    \\usepackage{amssymb}
+    \\usepackage{graphicx}
+    \\author{Antonio Molina Garca-Retamero}
+    \\title{Results report}
+    \\makeindex
+    \\begin{document}
+    \\maketitle
+    \\pagebreak
+    \\tableofcontents
+    \\pagebreak
+    \\section{Results for classification in clustered data set}
+    In this section we'll perform a test battery in order to determine how well works the neural network by classifying data in a clustered data set.
+    For each test, we test the accuracy of the prediction for a determine data set clustered in k centroids and the time elapsed in training phase. We will increase the number of neurons in first layer for each test
+    '''
+    for i in range(3):
+        latexReport += '''\\subsection{For %d clusters and %d samples}''' % (i+2, 1000)
+        cdt = ClusteredDataGenerator(1000, i+2, 2)
+        cdt.savePlot()
+        latexReport += '''\\subsubsection{Two first dimensions one over other} \\begin{figure}[!h]{}
+                            \\centering
+                            \\includegraphics[width=0.4\\textwidth]{plots/%dc%ds.png}
+                            \\label{fig:clusteredData1}
+                        \\end{figure}''' % (i+2, 1000)
+        nc = 50
+        nC = np.zeros(10)
+        perf = np.zeros(10)
+        tT = np.zeros(10)
+        for j in range(10):
+            rbfnn = RBFNN(2, nc, 1)
+            t = time.clock()
+            rbfnn.train(cdt.getTrainingX(), cdt.getTrainingY())
+            tT[j] = time.clock()-t
+            perf[j] = cdt.verifyResult(rbfnn.test(cdt.getValidationX()))
+            nC[j] = nc
+            nc += 50
+        
+        #Writing table of results
+        latexReport += '''\\begin{tabular}{|l | c | r|} \\hline Centroids & Performance & Training time \\\\ \\hline'''
+        for j in range(len(nC)):
+            latexReport += '''%d & %s & %s\\\\ \\hline''' % (nC[j], str(perf[j]), str(tT[j]))
+        latexReport += '''\\end{tabular}'''
+            
+        #Plotting accuracy over nCentroids
+        plt.clf()
+        plt.plot(nC, perf, "-r")
+        plt.savefig("plots/Accuracy%dc%ds.png" % (nc, 1000))
+        latexReport += '''\\subsubsection{Accuracy over number of centroids} \\begin{figure}[!h]{}
+                        \\centering
+                        \\includegraphics[width=0.4\\textwidth]{plots/Accuracy%dc%ds.png}
+                        \\label{fig:clusteredData1}
+                    \\end{figure}''' % (nc, 1000)
+        #Plotting time over nCentroids
+        plt.clf()
+        plt.plot(nC, tT, "-r")
+        plt.savefig("plots/Time%dc%ds.png" % (nc, 1000))
+        latexReport += '''\\subsubsection{Time required for training over number of centroids} \\begin{figure}[!h]{}
+                        \\centering
+                        \\includegraphics[width=0.4\\textwidth]{plots/Time%dc%ds.png}
+                        \\label{fig:clusteredData1}
+                    \\end{figure}''' % (nc, 1000)
+    latexReport += '''\\end{document}'''
+    with open('report.tex','w') as f:
+        f.write(latexReport.encode('utf8'))
+    proc=subprocess.Popen(shlex.split('pdflatex report.tex'))
+    proc.communicate()
+
 
 if __name__ == '__main__':
     print 'Performing test over the RBFNNs'
-    # Performing test based on clustered data
-    cdt = ClusteredDataGenerator(1000, 3, 2)
-#     cdt.plotDataSet()
-    rbfnn = RBFNN(2, 500, 1)
-    rbfnn.train(cdt.getX(), cdt.getY())
-    cdt.verifyResult(rbfnn.test(cdt.getX()))
+    ClusteredDataTest()
+    
