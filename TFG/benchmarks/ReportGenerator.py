97a57c8f0aa98aca3835e80b1032da3824f2b303
antonio <antonio@antonio-N53SV>
2014-11-12 11:41:06 +0100

    y lo que faltaba...

---
+++ TFG/benchmarks/ReportGenerator.py
@@ -0,0 +1,14 @@
+'''
+Created on Nov 12, 2014
+
+@author: antonio
+'''
+
+class ReportGenerator(object):
+    '''
+    Esta clase se expone una interfaz que facilita la generacion de informes sobre datos experimentales
+    '''
+
+    def __init__(self):
+        pass
+        
